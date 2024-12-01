08dd28156eae3b1bb7b96740c1d9c5a4e53c903f
ProgSomel <somelahmed55@gmail.com>
2024-12-01 20:33:18 +0600

    app and server setup done

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 const app = express();
+
+// middleWires 
+app.use(express.json());
+
+app.get('/', (req:Request, res:Response) => {
+    res.send({
+        status: true,
+        message: 'Welcome to the Car Store Server'
+    })
+})
+
+export default app;
